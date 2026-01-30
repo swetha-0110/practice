1b8aecbb1b39ab57b980013adfad4befe87db6e0
Swetha <swetha.aligety@gmail.com>
2026-01-30 15:00:09 +1300

    variables

---
+++ variablesandobjects.ts
@@ -0,0 +1,30 @@
+let designation: string
+let age: number
+let country: string
+
+const employeedetails={
+    FirstName:"swetha",
+    age:25,
+    empid:2345,
+    designation:"QA"
+
+};
+function greet()
+{
+    console.log("Hello all");
+}
+
+greet();
+
+function greet1(name:string)
+{
+    console.log("hello" + name);
+}
+greet1("swetha");
+
+
+
+
+
+
+
